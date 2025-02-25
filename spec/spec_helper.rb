58c98fa61f26497bdee52709b8876ad7efd91629
Kevin TAN <kevin.tan@alex.world>
2025-02-25 09:55:15 +0800

    Add SimpleCov for test coverage reporting
    - Add SimpleCov gem to test group in Gemfile
    - Configure SimpleCov in spec_helper.rb to:
      - Filter out spec/ directory
      - Filter out vendor/ directory
      - Generate JSON reports for Code Climate integration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,15 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'value_objects'
 require 'action_view'
+require 'simplecov'
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: ':memory:')
+
+SimpleCov.start do
+  add_filter '/spec/'
+  add_filter '/vendor/'
+  
+  # You can add more configuration here, like:
+  # enable_coverage :branch # for branch coverage
+  # minimum_coverage 90 # to enforce minimum coverage percentage
+end
